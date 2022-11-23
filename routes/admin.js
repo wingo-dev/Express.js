21a4ad61edc0b129fcaa45f7ff24852579ef9fec
= <rukshanlancer@gmail.com>
2022-11-23 22:38:07 +0800

    html addtion

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use("/add-product", (req, res, next) => {
-    res.send("<form action='/product' method='post'><input type='text' name='title'/><button type='submit'>add product</button></form>");
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req, req.body);
     res.redirect('/');
 });
