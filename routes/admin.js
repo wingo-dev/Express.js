ac2b2428adda11e8e5cc8b9212a6b9f38c969aa0
= <rukshanlancer@gmail.com>
2022-11-25 18:03:09 +0800

    pug(jade) template workflow

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,23 @@
 const path = require('path');
 
+const express = require('express');
+
 const rootDir = require('../util/path');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
 const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
+
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req, req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
