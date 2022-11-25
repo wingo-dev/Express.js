cec8f385caf26def7c61f793525396379e20899c
= <rukshanlancer@gmail.com>
2022-11-25 22:07:27 +0800

    handlebars template

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-// const http = require('http');
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+    'hbs',
+    expressHbs.engine({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -19,9 +27,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-// const server = http.createServer(app);
-// server.listen(3000);
 app.listen(3000);
