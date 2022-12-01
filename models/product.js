bddac9390537164314f47b3f70e2cf6e50079b43
= <rukshanlancer@gmail.com>
2022-12-02 02:58:48 +0800

    edit functionality

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,5 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
 module.exports = router;
