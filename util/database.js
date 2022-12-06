0110f0c13b17ca2acd919ca523ca499d2604699d
= <rukshanlancer@gmail.com>
2022-12-07 05:18:54 +0800

    replace database.js with sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', 
-    database: 'node_db',
-    password: ''
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root', 
+//     database: 'node_db',
+//     password: ''
+// });
+
+// module.exports = pool.promise();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node_db', 'root', '', {
+    dialect: 'mysql', 
+    host: 'localhost', 
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
